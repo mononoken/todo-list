5fbcdfd182aca134281a9243284ee4214bd2195f
mononoken <ken.maeshima@pm.me>
2024-04-17 16:15:22 -0700

    Create Todo model

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
 import "./style.css";
+import Todo from "./models/todo";
 
 const appContainer = document.querySelector<HTMLDivElement>("#app")!;
 appContainer.innerHTML = "Todo project";
+
+localStorage.clear();
+
+const testTodo = new Todo("Test", "this is a test", null);
+console.log(testTodo);
+
+const nextTestTodo = new Todo("Another Test", "this is also a test", null);
+console.log(nextTestTodo);
+
+const thirdTodo = new Todo("Third Test", "three is a magic number", null);
+console.log(thirdTodo);
+
+const allTodos = Todo.getAll();
+console.log(allTodos);
---
+++ src/models/todo.ts
@@ -0,0 +1,52 @@
+const storageLabel = "todos";
+const nextIDLabel = "nextTodoID";
+
+export default class Todo {
+  constructor(
+    public name: string,
+    public description: string,
+    public completed: Date | null,
+    public id: number = Todo.nextID(),
+  ) {
+    Todo.push(this);
+    localStorage.setItem(nextIDLabel, id + 1);
+  }
+
+  static getAll(): Todo[] {
+    const todos = localStorage.getItem(storageLabel);
+
+    if (todos === null) {
+      return [];
+    }
+
+    return JSON.parse(todos);
+  }
+
+  static get(id: number): Todo | undefined {
+    const todos = Todo.getAll();
+
+    return todos.find((todo) => todo.id === id);
+  }
+
+  static push(todo) {
+    const todos = Todo.getAll();
+
+    this.setAll(todos.concat(todo));
+  }
+
+  private static nextID(): number {
+    const id = localStorage.getItem(nextIDLabel);
+
+    if (id === null) {
+      localStorage.setItem(nextIDLabel, JSON.stringify(1));
+
+      return 1;
+    }
+
+    return JSON.parse(id);
+  }
+
+  private static setAll(todos: Todo[]) {
+    localStorage.setItem(storageLabel, JSON.stringify(todos));
+  }
+}
